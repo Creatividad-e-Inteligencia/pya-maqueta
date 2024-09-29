e7b86f05fe854a330e0addb0373a4584edf8d9b0
ivancho-ci <isaldia@creatividadeinteligencia.cl>
2024-09-29 16:36:52 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,179 @@
+$(function() {  
+
+  $(".cabecera-sitio .btn-movil").click(function(e){
+       e.preventDefault();
+       $(".cabecera-sitio .menu").toggleClass('active');
+     });
+// Select all links with hashes
+    $('a[href*="#"]')
+      // Remove links that don't actually link to anything
+      .not('[href="#"]')
+      .not('[href="#0"]')
+      .click(function (event) {
+        // On-page links
+        if (
+          location.pathname.replace(/^\//, "") ==
+            this.pathname.replace(/^\//, "") &&
+          location.hostname == this.hostname
+        ) {
+          event.preventDefault();
+          // Figure out element to scroll to
+          var target = $(this.hash);
+          target = target.length
+            ? target
+            : $("[name=" + this.hash.slice(1) + "]");
+          // Does a scroll target exist?
+          if (target.length) {
+            // Only prevent default if animation is actually gonna happen
+            event.preventDefault();
+            $("html, body").animate(
+              {
+                scrollTop: target.offset().top,
+              },
+              1000,
+              function () {
+                $( "#check" ).prop( "checked", false );
+              }
+            );
+          }
+        }
+      });	
+
+      $('.slider-banner').slick({
+        dots: true,
+        slidesToShow: 1,
+        arrows:false,
+        appendDots:'.banner-home .dots',
+      });
+      $('.slider-galeria').slick({
+        dots: true,
+        slidesToShow: 1,
+        arrows:false,
+        appendDots:'.dots',
+      });
+      $('.slider-novedades').slick({
+        dots: true,
+        slidesToShow: 3,
+        arrows:false,
+        appendDots:'.novedades .dots',
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          },
+        ]
+      });
+      $('.slider-clientes').slick({
+        dots: true,
+        slidesToShow: 5,
+        arrows:false,
+        appendDots:'.clientes .dots',
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          },
+        ]
+      });
+      $('.slider-asociaciones').slick({
+        dots: true,
+        slidesToShow: 5,
+        arrows:false,
+        appendDots:'.asociaciones .dots',
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          },
+        ]
+      });
+
+      function igualarAltura(clase) {
+               var alturas = 0;
+
+               // Recorre todos los elementos con la clase proporcionada
+               $('.' + clase).each(function() {
+                   // Restablece la altura para que se pueda calcular correctamente
+                   $(this).css('height', 'auto');
+                   // Compara y almacena la altura máxima
+                   alturas = Math.max(alturas, $(this).height());
+               });
+
+               // Establece la altura máxima en todos los elementos
+               $('.' + clase).height(alturas);
+           }
+
+           // Llama a la función al cargar la página y en el evento de cambio de tamaño de la ventana
+           $(document).ready(function() {
+               // Llama a la función con la clase 'elemento'
+               setTimeout(function() {
+              igualarAltura('cuadro-indicador');
+              igualarAltura('titulo-novedad');
+              igualarAltura('contenedor-cuadro-servicio');
+              igualarAltura('contenedor-categoria');
+
+
+            }, 100);
+
+               // Vuelve a llamar a la función cuando cambia el tamaño de la ventana
+               $(window).resize(function() {
+                   igualarAltura('cuadro-indicador');
+                   igualarAltura('titulo-novedad');
+                   igualarAltura('contenedor-cuadro-servicio');
+                   igualarAltura('contenedor-categoria');
+               });
+           });
+
+
+  }
+);
